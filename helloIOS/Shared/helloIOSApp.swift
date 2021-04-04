f8d0f8ab61b9f6bc1a7fc0d1573f7d59c5c0ab1f
Timothy Joo <timothyjoo@Timothys-MacBook-Pro.local>
2021-04-04 13:22:16 -0700

    Added basic files

---
+++ helloIOS/Shared/helloIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloIOSApp.swift
+//  Shared
+//
+//  Created by Timothy Joo on 4/4/21.
+//
+
+import SwiftUI
+
+@main
+struct helloIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
